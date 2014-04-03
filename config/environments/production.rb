9a9106ea7cab4088434d609189568a4c5b906d40
Sopheak089 <sopheak089269076@gmail.com>
2014-04-03 09:44:13 +0700

    update production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Myproject::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -65,7 +65,7 @@ Myproject::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # Enable locale fallbacks for I18n (makes lookups for any 97locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
 
